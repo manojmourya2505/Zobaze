758667f03ab3b25c2cb1034d1bdc73ae6caef08f
manoj-lohono <manoj.mourya@lohono.com>
2024-02-24 05:51:36 +0530

    task done.

--- app/src/main/java/com/zobaze/zobazerefractortask/MainActivity.kt
+++ app/src/main/java/com/zobaze/zobazerefractortask/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: EmployeeViewModel
     private lateinit var adapter: EmployeeAdapter
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
